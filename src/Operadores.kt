e09ea91ba4ce19ade6f3e781e6368edb4ce33e8d
Mario Monzón <hola@mariomonzon.dev>
2020-10-10 09:05:15 +0100

    Curso de Kotlin. Ejercicio Hola Mundo. Ejercicio Operadores Unarios y Binarios

---
+++ src/Operadores.kt
@@ -0,0 +1,32 @@
+import kotlin.time.milliseconds
+
+fun main() {
+
+    operadoresUnarios()
+    operadoresBinarios()
+}
+
+fun operadoresUnarios(){
+    val a: Int = 12
+    val b: Int = 3
+    val c: Boolean = true
+
+    println( "Operadores Unarios" )
+    println( a.unaryPlus() )    // +12
+    println( b.unaryMinus() )   // -3
+    println( a.inc() )          // 13
+    println( b.dec() )          // 2
+    println( c.not() )          // falsa
+}
+
+fun operadoresBinarios(){
+    val a: Int = 12
+    val b: Int = 3
+
+    println( "Operadores Binarios" )
+    println( a.plus(b) )    // 15
+    println( a.minus(b) )   // 9
+    println( a.times(b) )   // 36
+    println( a.div(b) )     // 4
+    println( a.rem(b) )     // 0
+}
